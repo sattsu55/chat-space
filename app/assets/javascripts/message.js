78bbce03bb5e4c764eee81516207833d7cbb97cb
sattsu55 <owner@ownernoMacBook-puro.local>
2019-01-27 13:41:05 +0900

    非同期化、フォーム削除までOK

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,31 @@
 $(function(){
-  $('#form').on('submit', function(e){
-    var formData = new FormData(this);
+  function buildHTML(message){
+    var html = `<p>
+                  ${message.content}
+                </p>`
+    return html;
+  }
+
+
+  $('.new_message').on('submit', function(e){
     e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__body').append(html)
+      $('.form__message').val('')
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
 })
