6a0abb7f36841bba61577f2bbec6f9a9a56a5f33
sattsu55 <owner@ownernoMacBook-puro.local>
2019-01-29 14:14:18 +0900

    ユーザーソートの修正
    ログインユーザーが検索結果に反映されないように
    不要なrespond記述の削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
-    respond_to do |format|
-     format.html
-     format.json
-    end
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
   end
 
   def edit
