588f6bdcf34f1be0d476f40b996a252a4cb52bca
sattsu55 <owner@ownernoMacBook-puro.local>
2019-02-08 17:01:41 +0900

    サイドバー修正、インクリメンタル

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
+    unless params[:keyword] == ""
+      @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
+    end
   end
 
   def edit
