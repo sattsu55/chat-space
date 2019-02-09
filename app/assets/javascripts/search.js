f01d391bca30b73a7a7a15d5a58c719f7787ebbe
sattsu55 <owner@ownernoMacBook-puro.local>
2019-02-09 19:06:09 +0900

    該当の人がいません

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,16 +1,12 @@
 $(function() {
 
-var search_result = $("#user-search-result");
-
 function appendUser(users){
   var html = `<div class="chat-group-user clearfix">
   <p class="chat-group-user__name">${users.name}</p>
   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${users.id}" data-user-name="${users.name}">追加</a>
 </div>`
-  search_result.append(html);
+  $("#user-search-result").append(html);
 }
-
-var chat_group_users = $('#chat-group-users');
 
 function appendUserToMember(users){
   var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${users.id}'>
@@ -18,7 +14,14 @@ function appendUserToMember(users){
   <p class='chat-group-user__name'>${users.name}</p>
   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
 </div>`
-  chat_group_users.append(html);
+  $('#chat-group-users').append(html);
+}
+
+function appendUsermiss(){
+  var html = `<div class="chat-group-user clearfix">
+  <p class="chat-group-user__name">該当の人はいません</p>
+</div>`
+  $("#user-search-result").append(html);
 }
 
   $("#user-search-field").on("keyup", function() {
@@ -35,6 +38,8 @@ function appendUserToMember(users){
         users.forEach(function(users){
           appendUser(users);
         });
+      }else{
+        appendUsermiss();
       }
       $('#user-search-result').ready(function(){
         $(".chat-group-user__btn--add").on("click",function(){
